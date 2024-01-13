5485dbfc2b59a750e7107015b0d8b9bce29c89a2
ravindu <ravindu7777prakash@gmail.com>
2024-01-13 23:23:24 +0530

    git4

---
+++ src/class3.java
@@ -0,0 +1,12 @@
+public class class3 {
+    int a;
+    boolean b;
+
+
+    public static void main(String[] args) {
+    class3 t=new class3();
+        System.out.println("int default value is "+t.a);
+        System.out.println("boolean default value "+t.b);
+
+    }
+}
